82d4341b4a5104eb20f895f4acffdf1aeaeed2fa
Jeremy Bokobza <bokobza@gmail.com>
2017-07-25 10:39:22 +0100

    Fixed references

--- Breeze.TumbleBit.Client/Models/SignVoucherRequest.cs
+++ Breeze.TumbleBit.Client/Models/SignVoucherRequest.cs
@@ -3,13 +3,13 @@ using NTumbleBit.ClassicTumbler;
 
 namespace Breeze.TumbleBit.Models
 {
-	public class SignVoucherRequest
-	{
-		public int KeyReference { get; set; }
+    public class SignVoucherRequest
+    {
+        public int KeyReference { get; set; }
 
-		public PubKey TumblerEscrowPubKey { get; set; }
+        public PubKey TumblerEscrowPubKey { get; set; }
 
-        public ClientEscrowInformation ClientEscrowInformation { get; set; }
+       // public ClientEscrowInformation ClientEscrowInformation { get; set; }
 
         public MerkleBlock MerkleProof { get; set; }
 
