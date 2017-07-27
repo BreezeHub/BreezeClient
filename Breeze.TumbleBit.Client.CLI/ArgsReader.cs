b5e9aa5253b26e4a9ca04cf4bf86766139ab171a
carlton355 <carlton.pringle@gmail.com>
2017-07-28 00:35:39 +0700

    Started building CLI

---
+++ Breeze.TumbleBit.Client.CLI/ArgsReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NBitcoin;
+
+namespace Breeze.TumbleBit.Client.CLI
+{
+	/// <summary>
+	/// ArgsReader gets parameters from the 
+	/// </summary>
+    internal sealed class ArgsReader
+	{
+		public string UriString { get; } = String.Empty;
+	    public string OriginWalletName { get; } = String.Empty;
+		public string DestinationWalletName { get; } = String.Empty;
+
+		public ArgsReader(string[] args)
+		{
+			this.UriString = this.GetArgument(args, "-ctb");
+			this.OriginWalletName = this.GetArgument(args, "-origin");
+			this.DestinationWalletName = this.GetArgument(args, "-destination");
+		}
+
+	    public bool VerifyArgs()
+	    {
+		    if (this.UriString == "xxxx")
+			    throw new Exception("If you are debugging set command line arguments in Project properties in the Debug Tab.");
+		    
+			if (!this.UriString.StartsWith("ctb"))
+		    {
+			    Console.WriteLine("The -ctb parameter is required and needs to be a ctb uri.");
+			    return false;
+		    }
+
+		    if(this.OriginWalletName == string.Empty|this.DestinationWalletName == string.Empty) {
+			    Console.WriteLine("Both the -origin and -destination parameters are required.");
+			    return false;
+		    }
+		    return true;
+	    }
+
+		string GetArgument(IEnumerable<string> args, string option)
+		    => args.SkipWhile(i => i != option).Skip(1).Take(1).FirstOrDefault();
+	}
+}
---
+++ Breeze.TumbleBit.Client.CLI/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NBitcoin;
+using NTumbleBit.Logging;
+using Stratis.Bitcoin;
+using Stratis.Bitcoin.Builder;
+using Stratis.Bitcoin.Configuration;
+
+namespace Breeze.TumbleBit.Client.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        FuncLoggerFactory loggerFactory =
+		        new FuncLoggerFactory(i => new CustomerConsoleLogger(i, (a, b) => true, false));
+			Logs.Configure(loggerFactory);
+
+			ArgsReader argsReader = new ArgsReader(args);
+	        if (!argsReader.VerifyArgs()) return;
+
+			//we don't want to be anywhere near MainNet
+	        if (NodeSettings.PrintHelp(args, Network.Main))
+		        return;
+
+	        Uri tumblerUri = new Uri(argsReader.UriString);
+
+			//Start the engines!
+			NodeSettings nodeSettings = NodeSettings.FromArguments(args);
+			FullNode fullNode = StartupFullNode(nodeSettings, tumblerUri);
+
+			ITumbleBitManager tumbleBitManager;// = new TumbleBitManager(loggerFactory, );
+
+			//use the tumblebitManager with a wait
+	        //tumbleBitManager.ConnectToTumblerAsync(tumblerUri).GetAwaiter().GetResult();
+
+			//we don't want to do this as it will lock up the console...
+			//tumbleBitManager.TumbleAsync(argsReader.OriginWalletName, argsReader.DestinationWalletName).GetAwaiter().GetResult();
+        }
+
+	    private static FullNode StartupFullNode(NodeSettings nodeSettings, Uri tumblerUri)
+	    {
+		    //var fullNodeBuilder = new FullNodeBuilder()
+			   // .UseNodeSettings(nodeSettings)
+			   // .UseLightWallet()
+			   // .UseBlockNotification()
+			   // .UseTransactionNotification()
+			   // .UseApi()
+			   // .UseTumbleBit(tumblerUri)
+			   // .UseWatchOnlyWallet()
+			   // .Build();
+
+		    //return fullNodeBuilder;
+		    return null;
+	    }
+    }
+}
